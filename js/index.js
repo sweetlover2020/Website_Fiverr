df70fc7ce33dda524e142ab0096ff9afe9c96a83
Đặng Phúc Minh <s2vethan2s@gmail.com>
2020-11-15 12:09:47 +0700

    Minh_V2.6

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+$(".js-video-button").modalVideo({
+    youtube: {
+        controls: 0,
+        nocookie: true
+    }
+});
+
+// <!--Carousel - students-- >
+$('.comment__carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        // 600: {
+        //     items: 3
+        // },
+        // 1000: {
+        //     items: 4
+        // },
+        // 1440: {
+        //     items: 6
+        // },
+    }
+})
+// Project Carousel
+$('.project__carousel').owlCarousel({
+    items:4,
+    nav: true,
+    loop:true,
+    margin:10,
+    merge:true,
+    dots: false,
+    responsive:{
+        678:{
+            mergeFit:true
+        },
+        1000:{
+            mergeFit:false
+        }
+    }
+})
