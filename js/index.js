2b0d932f9ef85e73d897ef52ca0b27fa30d492b5
Đặng Phúc Minh <s2vethan2s@gmail.com>
2020-11-17 14:51:29 +0700

    'Minh_4.4'

--- js/index.js
+++ js/index.js
@@ -61,22 +61,25 @@ $('.services__carousel').owlCarousel({
         }
     }
 })
+// Service Carousel
+$('.service__carousel').owlCarousel({
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    merge:true,
+    loop: true,
+    margin: 10,
+    nav: true,
     dots: false,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
-        }
+        1000: {
+            items: 4
+        },
+        1440: {
+            items: 6
+        },
     }
 })
