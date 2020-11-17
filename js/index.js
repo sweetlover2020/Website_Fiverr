84e7c1372a7350c5230cab2c2864bc78cf69005b
Đặng Phúc Minh <s2vethan2s@gmail.com>
2020-11-17 22:31:47 +0700

    'Update_4.5'

--- js/index.js
+++ js/index.js
@@ -6,30 +6,6 @@ $(".js-video-button").modalVideo({
     }
 });
 
-// // Carousel-students
-// $('.services__carousel').owlCarousel({
-
-//     loop: true,
-//     margin: 10,
-//     nav: true,
-//     dots: false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 3
-//         },
-//         1000: {
-//             items: 5
-//         },
-//         // 1440: {
-//         //     items: 6
-//         // },
-//     }
-// })
-
-
 // <!--Carousel - students-- >
 $('.comment__carousel').owlCarousel({
     loop: true,
@@ -94,4 +70,47 @@ $('.service__carousel').owlCarousel({
             items: 5
         },
     }
+})
+// Scroll menu layer 2
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 1) {
+        $('header').addClass('headerClass1');
+    } else {
+        $('header').removeClass('headerClass1');
+    };
+
+    // Đổi màu chữ
+    if ($(window).scrollTop() > 1) {
+        $('.header__left').addClass('change_color1');
+    } else {
+        $('.header__left').removeClass('change_color1');
+    };
+    if ($(window).scrollTop() > 1) {
+        $('.fiver__menu').addClass('change_color2');
+    } else {
+        $('.fiver__menu').removeClass('change_color2');
+    };
+
+    // Hiện input group
+    if ($(window).scrollTop() > 1) {
+        $('.input-group').addClass('visibleInput');
+    } else {
+        $('.input-group').removeClass('visibleInput');
+    };
+
+    // Hiện Menu layer 2
+    if ($(window).scrollTop() > 400) {
+        $('.menu_layer2').addClass('headerClass');
+    } else {
+        $('.menu_layer2').removeClass('headerClass');
+    };
+
+    // Thêm viền cho thẻ hr
+    if ($(window).scrollTop() > 400) {
+        $('header').addClass('headerHr');
+    } else {
+        $('header').removeClass('headerHr');
+    };
+
+
 })
